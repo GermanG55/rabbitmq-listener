09163711938ae462a38353b00d3b199ff1c19b22
GermanG55 <grmnrtr@gmail.com>
2023-05-05 19:46:59 -0500

    "borrar cosas que no sirven pero no son requerimientos de la tarea"

--- src/main/java/com/example/demo/rabbitmq/ConfigurarRabbitmq.java
+++ src/main/java/com/example/demo/rabbitmq/ConfigurarRabbitmq.java
@@ -6,16 +6,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigurarRabbitmq {
-    @Bean
-    Queue crearCola(){
-        return new Queue("procesosCola", false);
-    }
-    @Bean
-    TopicExchange exchange(){
-        return new TopicExchange("colaCambio");
-    }
-    @Bean
-    Binding unir(Queue cola, TopicExchange exchange){
-        return BindingBuilder.bind(cola).to(exchange).with("unir.#");
-    }
+
 }
